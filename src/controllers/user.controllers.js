8799d9b2b6f5734d5fb58e9c148756caef54a0d6
Fabian_Avila <116823446+fabian-4vila@users.noreply.github.com>
2023-10-30 12:45:13 -0500

    corrigiendo el update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -30,7 +30,7 @@ const updateUser = catchError(async(req,res) => {
         { email, password, first_name, last_name, birthday },
         {where: { id:id }, returning: true}
     );
-    return res.json(user);
+    return res.json(user[1][0]);
 })
 
 module.exports={
